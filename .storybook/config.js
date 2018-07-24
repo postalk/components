e87616f187f2dd126db0aa0decfe8aca4697b0d0
Kiyohiko Heima <piyo326@gmail.com>
2018-07-25 04:25:51 +0900

    📕 Fix storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,8 +2,14 @@ import path from 'path'
 import requireContext from 'require-context'
 import { configure } from '@storybook/vue'
 
-// automatically import all files ending in *.stories.js
-const req = requireContext(path.join(__dirname, '../src'), true, /.story.ts$/)
+let req
+
+if (process.env.STORY_ENV === 'test') {
+  req = requireContext(path.join(__dirname, '../src'), true, /.story.ts$/)
+} else {
+  req = require.context('../src', true, /.story.ts$/)
+}
+
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
