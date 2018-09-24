df1d4da85b43037afd9f36cf88189740574b9048
Kiyohiko Heima <piyo326@gmail.com>
2018-09-24 22:14:40 +0900

    🗓 Add table

--- src/components/Card/list.vue
+++ src/components/Card/list.vue
@@ -26,19 +26,13 @@ export default class List extends Vue {
   list-style-type: none;
   line-height: 1.5;
   padding-left: 0;
-  margin-top: 0;
-  margin-bottom: -0.5rem;
-  margin-left: -0.5rem;
-  margin-right: -0.5rem;
+  margin: -0.5rem;
   font-size: 0.875rem;
 }
 .item {
-  padding-bottom: 0.5rem;
-  padding-left: 0.5rem;
-  padding-right: 0.5rem;
+  padding: 0.5rem;
   & + .item {
     border-top: 1px solid rgba(0, 0, 0, 0.05);
-    padding-top: 0.5rem;
   }
 }
 </style>
